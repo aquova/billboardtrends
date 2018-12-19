7c32d4a0a885152abb42e2a387b6af02575b0945
Ethan Jaynes <ethandjay@gmail.com>
2018-12-19 16:01:47 -0600

    circumvent pop up blocker in click to music

--- js/stream.js
+++ js/stream.js
@@ -190,10 +190,12 @@ function constructStream() {
         if (query.includes('/')) {
             query = d.key.split('/')[Math.round(Math.random())];
         }
+
+        var win = window.open();
         $.ajax({
             url: `https://cors-anywhere.herokuapp.com/https://www.youtube.com/results?search_query=${Math.floor(xScale.invert(event.pageX-60))}+${query}`,
             success: function(resp) {
-                window.open(`https://www.youtube.com/${resp.match(/watch\?v=.*?"/)}`)
+                win.location.href = `https://www.youtube.com/${resp.match(/watch\?v=.*?"/)}`
             }
         });
 
